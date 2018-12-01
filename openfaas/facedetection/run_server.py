65377ef547e412c31e6331e28ab9628f7fbb9f68
Dmitry Kozlov <dmitry.f.kozlov@gmail.com>
2018-12-01 11:14:24 +0300

    update run_server

--- openfaas/facedetection/run_server.py
+++ openfaas/facedetection/run_server.py
@@ -1,11 +1,27 @@
 from flask import Flask, request, jsonify
 from handler import handle
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
-def main_route():
+@app.before_request
+def fix_transfer_encoding():
+    """
+    Sets the "wsgi.input_terminated" environment flag, thus enabling
+    Werkzeug to pass chunked requests as streams.  The gunicorn server
+    should set this, but it's not yet been implemented.
+    """
+
+    transfer_encoding = request.headers.get("Transfer-Encoding", None)
+    if transfer_encoding == u"chunked":
+        request.environ["wsgi.input_terminated"] = True
+
+@app.route("/", defaults={"path": ""}, methods=["POST", "GET"])
+@app.route("/<path:path>", methods=["POST", "GET"])
+def main_route(path):
     return handle(request.get_data())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    #app.run(host='0.0.0.0', port=5000, debug=False)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
